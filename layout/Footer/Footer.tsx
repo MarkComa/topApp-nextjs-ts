b3bd1a114efad88e003992abeb2116d98bc68892
markcoma <dex.go@yandex.ru>
2022-03-24 23:21:21 +0300

    footer css

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { FooterProps } from './Footer.props'
 import s from "./Footer.module.css"
 import cn from 'classnames'
+import { format } from 'date-fns';
 
 export const Footer = ({children,className, ...props}:FooterProps):JSX.Element => {
   return (
     <footer className={cn(className, s.footer)}{...props}>
-      <p className={s.p}>OwlTop © 2020 - 2021 Все права защищены</p>
-      <p className={s.p}>Пользовательское соглашение</p>
-      <p className={s.p}>Политика конфиденциальности</p>     
+      <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+      <a href='#' target='_blank'>Пользовательское соглашение</a>
+      <a href='#' target='_blank'>Политика конфиденциальности</a>     
     </footer>
   )
 }
